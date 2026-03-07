19888658ca6cc3cc99b9e6320a086da245b79d21
Gerardo Miranda <gerardodev25@gmail.com>
2026-03-07 17:42:58 -0400

    chore(work): :construction_worker: add pdfmake and send our first pdf

--- src/basic-reports/basic-reports.service.ts
+++ src/basic-reports/basic-reports.service.ts
@@ -1,12 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import PdfPrinter from 'pdfmake';
+import { TDocumentDefinitions } from 'pdfmake/interfaces';
 
+const fonts = {
+  Roboto: {
+    normal: 'fonts/Roboto-Regular.ttf',
+    bold: 'fonts/Roboto-Medium.ttf',
+    italics: 'fonts/Roboto-Italic.ttf',
+    bolditalics: 'fonts/Roboto-MediumItalic.ttf',
+  },
+};
 @Injectable()
 export class BasicReportsService {
   constructor(private prisma: PrismaService) {}
 
-  async hello() {
-    const employees = await this.prisma.employees.findMany();
-    return employees;
+  hello() {
+    const printer = new PdfPrinter(fonts);
+    const documentDefinition: TDocumentDefinitions = {
+      content: ['hello world'],
+    };
+    const doc = printer.createPdfKitDocument(documentDefinition);
+    return doc;
   }
 }
